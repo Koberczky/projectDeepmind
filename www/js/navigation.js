0b23b49d643ea619251ca833a908243fab7ad1ac
schuman0 <mattschuman43@gmail.com>
2014-04-07 13:13:27 -0500

    Draw polyline

--- www/js/navigation.js
+++ www/js/navigation.js
@@ -1,10 +1,21 @@
 function getNavigation (map, originLat, originLon, destLat, destLon){
+var nav = [
+ new google.maps.LatLng(originLat, originLon),
+ new google.maps.LatLng(destLat, destLon)
+]
 var origin = new Array();
 var destination = new Array();
 origin[0] = originLat;
 origin[1] = originLon;
 destination[0] = destLat;
 destination[1] = destLon;
+map.drawPolyline({
+    path: nav,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
 map.drawRoute({
 origin: origin,
 destination: destination,
