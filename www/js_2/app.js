791b843e71d05b8ce567760e33a35cfec888b684
schuman0 <mattschuman43@gmail.com>
2014-04-26 22:22:14 -0500

    Removed extra js file

--- www/js_2/app.js
+++
@@ -1,9 +0,0 @@
-$(document).foundation();
-function goBack() {
-    if (document.getElementById('infowindow').style.visibility==="visible"){
-        document.getElementById('infowindow').style.visibility="hidden";
-    }
-    else{
-    window.history.back();
-    }
-}
