37d13326b4a2c68c2ba89337c10835d06eed5cd7
schuman0 <mattschuman43@gmail.com>
2014-04-27 20:59:15 -0500

    Updated Navigation. Updated images.

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,13 @@
 $(document).foundation();
+
 function goBack() {
-    if (document.getElementById('infowindow').style.visibility==="visible"){
-        document.getElementById('infowindow').style.visibility="hidden";
-    }
-    else{
-    window.history.back();
+    if (document.getElementById('infowindow')) {
+        if (document.getElementById('infowindow').style.visibility === "visible") {
+            document.getElementById('infowindow').style.visibility = "hidden";
+        } else {
+            window.history.back();
+        }
+    } else {
+        window.history.back();
     }
 }
