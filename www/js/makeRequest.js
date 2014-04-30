89b0a7ee02a9fde90bc315db38135a5cababbdbd
schuman0 <mattschuman43@gmail.com>
2014-04-29 19:37:46 -0500

    Nav and image updates from Monday

--- www/js/makeRequest.js
+++ www/js/makeRequest.js
@@ -1,21 +1,21 @@
-function makeRequest(callbackfunction){
-var retdata;
-   $.ajax({
-    type:"GET",
+function makeRequest(callbackfunction) {
+    var retdata;
+    $.ajax({
+        type: "GET",
         url: 'http://www.greenville.edu/application/api/buildingjsonemitter.html',
-   dataType:'jsonp',
-        success: function(data){
+        dataType: 'jsonp',
+        success: function(data) {
             retdata = data;
             var jsonString = "";
-            data.contentlets.forEach(function(building){
-            jsonString += "<strong>" + building.title + "</strong>";
-            jsonString += building.description;
+            data.contentlets.forEach(function(building) {
+                jsonString += "<strong>" + building.title + "</strong>";
+                jsonString += building.description;
             });
-        callbackfunction(data);
+            callbackfunction(data);
         },
-        error: function(jqXHR, status, exception){
-        console.log(status + " " + exception);
-        console.log(jqXHR);
+        error: function(jqXHR, status, exception) {
+            console.log(status + " " + exception);
+            console.log(jqXHR);
         }
     });
 }
