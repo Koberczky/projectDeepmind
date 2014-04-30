3889272e258a9fd6e9cce8671c41f9b340c381ac
schuman0 <mattschuman43@gmail.com>
2014-04-30 10:49:52 -0500

    Fixed scrolling. Updated css.

--- www/js/navigation.js
+++ www/js/navigation.js
@@ -1,78 +1,170 @@
 var imgArray = new Array();
-var i = 1;
-
-function getImages() {
-    while (i < 48) {
-        imgArray[i] = "img/navImages/img" + i + ".jpg";
-        ++i;
-    }
-    return imgArray;
-}
-
-function guidedNav(data) {
-
-
-    if (navigator.geolocation) {
-        var watchID = navigator.geolocation.watchPosition(onSuccess, onError, {
-            enableHighAccuracy: true,
-            timeout: 30000
-        });
-        //navigator.geolocation.getCurrentPosition(onSuccess, onError, { enableHighAccuracy:true });
-    } else {}
-
-}
+var i = 0;
 var outOfBounds = 0;
 var j = 0;
 var eventListener = 0;
 var imgArray = getImages();
 var imageNum;
+var buildingNum = 0;
+var isBuilding = 0;
+
+function getImages() {
+    while (i < 46) {
+        imgArray[i] = "img/navImages/img" + i + ".jpg";
+        ++i;
+    }
+    return imgArray;
+}
+var watchID
+var buildingInfo = new Array();
+
+function guidedNav(data) {
+    if (window.sessionStorage.getItem("buildingNumber") != null) {
+        sessvars.data = data;
+        document.getElementById('navImage').innerHTML = "<p>It looks like you didn't finish your last tour, if you would like to restart from where you left off, <a href= 'javascript:void(0)' onclick = 'RestartTour()'>click here</a>.</p><p>Otherwise, <a href='javascript:void(0)' onclick = 'clearTour()'>click here to start from the beginning</a>";
+        return;
+    } else {
+        data.contentlets.forEach(function(building) {
+            if (building.fullTitle == "Whitlock Music Center") {
+                buildingInfo[0] = building;
+            }
+            if (building.fullTitle == "Marston Hall") {
+                buildingInfo[1] = building;
+            }
+            if (building.fullTitle == "LaDue Education Center & Auditorium") {
+                buildingInfo[2] = building;
+            }
+            if (building.fullTitle == "Ruby E. Dare Library") {
+                buildingInfo[3] = building;
+            }
+            if (building.fullTitle == "Glen and Maxine Crum Recreation Center") {
+                buildingInfo[4] = building;
+            }
+            if (building.fullTitle == "Dietzman Center") {
+                buildingInfo[5] = building;
+            }
+            if (building.fullTitle == "Carrie T. Burritt Hall-Burritt Annex") {
+                buildingInfo[6] = building;
+            }
+            if (building.fullTitle == "Armington Center") {
+                buildingInfo[7] = building;
+            }
+            if (building.fullTitle == "Scott Field") {
+                buildingInfo[8] = building;
+            }
+            if (building.fullTitle == "Snyder Hall of Science") {
+                buildingInfo[9] = building;
+            }
+            if (building.fullTitle == "Janssen Hall") {
+                buildingInfo[10] = building;
+            }
+            if (building.fullTitle == "Walter A. Joy Hall") {
+                buildingInfo[11] = building;
+            }
+            if (building.fullTitle == "Mailroom") {
+                buildingInfo[12] = building;
+            }
+            if (building.fullTitle == "Sims Student Union") {
+                buildingInfo[13] = building;
+            }
+        });
+
+        if (navigator.geolocation) {
+            watchID = navigator.geolocation.watchPosition(onSuccess, onError, {
+                enableHighAccuracy: true,
+                timeout: 30000
+            });
+            //navigator.geolocation.getCurrentPosition(onSuccess, onError, { enableHighAccuracy:true });
+        } else {}
+    }
+}
+
+function clearTour(data) {
+
+    sessionStorage.removeItem("buildingNumber");
+    guidedNav(sessvars.data);
+    return;
+}
+
+function RestartTour(data) {
+    buildingNum = sessionStorage.getItem("buildingNumber");
+    sessionStorage.removeItem("buildingNumber");
+    guidedNav(sessvars.data);
+    return;
+}
+
 
 function onSuccess(position) {
     var currentPos = [position.coords.latitude, position.coords.longitude];
     findImage(currentPos);
-    if (imageNum != null) {
-        var imageLink = "<img src='" + imgArray[imageNum] + "'>";
-        document.getElementById('navImage').innerHTML = imageLink;
-    }
 }
 
 function findImage(currentPos) {
-    var coordCompare = .002;
-    for (k = 0; k < 48; k++) {
+    var coordCompare = .502;
+    for (k = buildPosArray[buildingNum][0]; k <= buildPosArray[buildingNum][1]; k++) {
         var coordCalc = Math.abs(currentPos[1] - positionArray[k][1]) + Math.abs(currentPos[0] - positionArray[k][0]);
         if (coordCalc < coordCompare) {
             coordCompare = coordCalc;
             imageNum = k;
+            if (k == buildPosArray[buildingNum][1]) {
+                enterBuilding();
+                isBuilding = 1;
+            }
         }
+    }
+    if (coordCompare == .502) {
+        if (buildingNum < 13) {
+            document.getElementById('navImage').innerHTML = "<p>I noticed you've moved pretty far off path, if would you like to skip visiting " + buildingInfo[buildingNum].fullTitle + " then <a href ='javascript:void(0)' onclick = 'advanceBuilding()'>click here</a>.</p><p>If not, please make your way towards the building and the tour will automatically start back up.</p>";
+        } else {
+            document.getElementById('navImage').innerHTML = "<p>Please visit the Admisssions office to finish the tour</p>";
+        }
+    }
+    if (imageNum != null) {
+        var imageLink = "<img src = '" + imgArray[imageNum] + "'> ";
+        document.getElementById('navImage').innerHTML = imageLink;
+    }
+    if (isBuilding == 1) {
+        document.getElementById('buildingInfo').innerHTML = " <p> Do you want to enter " + buildingInfo[buildingNum].fullTitle + "?<br /><a href = 'javascript:void(0)' onclick = 'waitBuilding()'> Yes </a> <a href='javascript:void(0)' onclick='leaveBuilding()'>No</a> </p>";
+        isBuilding = 0;
     }
 
 }
 
+function enterBuilding() {
+    navigator.geolocation.clearWatch(watchID);
+}
 
-/*
-
-
-if (j < 47) {
-    var coordCalc = Math.abs(currentPos[1] - positionArray[j][1]) + Math.abs(currentPos[0] - positionArray[j][0]);
-
-    if (currentPos[0] > positionArray[j][0] - .00006 && currentPos[0] < positionArray[j][0] + .00006 && currentPos[1] > positionArray[j][1] - .00005 && currentPos[1] < positionArray[j][1] + .00005) {
-        outOfBounds = 0;
-        var imageLink = "<img src='" + imgArray[j] + "'>";
-        document.getElementById('navImage').innerHTML = imageLink;
-        ++j;
-    } else {
-
-        ++j;
-        window.setTimeout(onSuccess(position), 1000);
-        return;
+function waitBuilding() {
+    if (buildingNum == 14) {
+        document.getElementById('buildingInfo').innerHTML = "<p>You have finished the tour. Please head to the admissions office for further instructions</p> ";
     }
-} else {
-    resetCounter();
-    return;
-}*/
+    nextBuildingNum = parseInt(buildingNum) + 1;
+    document.getElementById('navImage').innerHTML = "<div class='overthrow buildingDesc'>" + buildingInfo[buildingNum].description + "</div>";
+    document.getElementById('buildingInfo').innerHTML = " <p id = 'enterBuilding'> <a href = 'javascript:void(0)' onclick = 'leaveBuilding()'> Click when you're ready to go to " + buildingInfo[nextBuildingNum].fullTitle + "</a></p>";
+}
+
+function leaveBuilding() {
+    if (buildingNum == 14) {
+
+    }
+    document.getElementById('buildingInfo').innerHTML = "";
+    ++buildingNum;
+    window.sessionStorage.setItem("buildingNumber", buildingNum);
+    watchID = navigator.geolocation.watchPosition(onSuccess, onError, {
+        enableHighAccuracy: true,
+        timeout: 30000
+    });
+}
+
+function advanceBuilding() {
+    ++buildingNum;
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+        enableHighAccuracy: true
+    });
+}
 
 function onError(error) {
-    alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+    alert('code: ' + error.code + '\n ' + ' message: ' + error.message + '\n ');
 }
 
 function resetCounter() {
@@ -81,7 +173,63 @@ function resetCounter() {
 
 function alertCoords(position) {}
 
+function orientationChange(e) {
+    var orientation = "portrait";
+    if (window.orientation == -90 || window.orientation == 90) orientation = "landscape";
+    if (orientation == "portrait") {
+        if ($("#navImage").hasClass("portrait")) {
 
+        } else {
+            if ($("#navImage").hasClass("landscape")) {
+                $("#navImage").removeClass("landscape");
+            }
+            $("#navImage").addClass("portrait");
+        }
+        if ($("#buildingInfo").hasClass("portrait")) {
+
+        } else {
+            if ($("#buildingInfo").hasClass("landscape")) {
+                $("#buildingInfo").removeClass("landscape");
+            }
+            $("#buildingInfo").addClass("portrait");
+        }
+    } else {
+        if ($("#navImage").hasClass("landscape")) {
+
+        } else {
+            if ($("#navImage").hasClass("portrait")) {
+                $("#navImage").removeClass("portrait");
+            }
+            $("#navImage").addClass("landscape");
+        }
+        if ($("#buildingInfo").hasClass("landscape")) {
+
+        } else {
+            if ($("#buildingInfo").hasClass("portrait")) {
+                $("#buildingInfo").removeClass("portrait");
+            }
+            $("#buildingInfo").addClass("landscape");
+        }
+    }
+}
+
+var buildPosArray = [
+    [0, 6],
+    [7, 9],
+    [10, 12],
+    [13, 16],
+    [17, 18],
+    [19, 24],
+    [15, 27],
+    [28, 29],
+    [30, 33],
+    [34, 34],
+    [35, 36],
+    [37, 37],
+    [38, 39],
+    [40, 40],
+    [41, 43]
+];
 
 var positionArray = [
     [38.892608, -89.409455],
@@ -92,7 +240,7 @@ var positionArray = [
     [38.892719, -89.408422],
     [38.892715, -89.408071],
     [38.892670, -89.407960],
-    [38.892938, -89.407998],
+    [38.893130, -89.407999],
     [38.893241, -89.408089],
     [38.893239, -89.408354],
     [38.893341, -89.408364],
@@ -107,9 +255,6 @@ var positionArray = [
     [38.894383, -89.409177],
     [38.894724, -89.409251],
     [38.894893, -89.409221],
-    [38.895070, -89.409223],
-    [38.895169, -89.409122],
-    [38.895133, -89.409143],
     [38.895201, -89.409212],
     [38.895305, -89.409184],
     [38.895298, -89.409271],
@@ -130,5 +275,5 @@ var positionArray = [
     [38.893117, -89.409455],
     [38.893071, -89.409417],
     [38.892860, -89.409446],
-    [38.892678, -89.409420],
+    [38.892678, -89.409420]
 ];
