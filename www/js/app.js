90cb452d16d72954e148df70f613542b66fa24f9
admin <200906154@panthers.greenville.edu>
2014-05-04 16:30:02 -0500

    worked on issues

--- www/js/app.js
+++ www/js/app.js
@@ -7,3 +7,12 @@ function goBack() {
     window.history.back();
     }
 }
+function buildingBack(){
+	if (document.getElementById('infowindow').style.visibility==="visible"){
+		document.getElementById('infowindow').style.visibility="hidden";
+		document.getElementById('buildings').style.visibility="visible";
+	}
+	else{
+		window.history.back();
+	}
+}
